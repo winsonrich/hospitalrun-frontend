6f8d8ba86868903c7d158afb9fc16ae2495237c2
Andrey Stukalin <apstukalin@gmail.com>
2018-03-22 09:36:35 +0100

    not finished: fix sample custom form creation

--- tests/helpers/scenarios/custom-forms.js
+++ tests/helpers/scenarios/custom-forms.js
@@ -6,97 +6,108 @@ const toppings =  ['Cheese', 'Pepperoni', 'Mushrooms'];
 const header = ['______________________________'];
 
 Ember.Test.registerAsyncHelper('createCustomFormForType', function(app, formType, alwaysInclude, assert) {
-  function addField(fieldType, label, values) {
-    click('button:contains(Add Field)');
-    waitToAppear('.modal-dialog');
-    andThen(function() {
-      if (assert) {
-        assert.equal(find('.modal-title').text(), 'Add Value', 'Add Value modal displays.');
-      }
 
-      select('.custom-field-select', fieldType);
-      fillIn('.custom-field-label input', label);
-      fillIn('.custom-field-colspan input', '1');
-    });
-    if (values) {
-      if (assert) {
-        click('button:contains(Add Value)');
-        andThen(function() {
-          fillIn('.custom-field-value:last', 'Delete Me');
-        });
-        andThen(function() {
-          assert.equal(find('.custom-field-value:contains(Delete Me)').length, 0, 'Field value successfully added');
-          click('button.delete-field-value');
-        });
-        andThen(function() {
-          assert.equal(find('.custom-field-value:contains(Delete Me)').length, 0, 'Field value successfully deleted');
-        });
-      }
-      andThen(function() {
-        values.forEach(function(value) {
-          click('button:contains(Add Value)');
-          andThen(function() {
-            fillIn('.custom-field-value:last', value);
-          });
-        });
-      });
-    }
-    andThen(function() {
-      click('.modal-footer button:contains(Add)');
-    });
-  }
+  let store = app.__container__.lookup('service:store');
+  let cf = store.createRecord('custom-form', {
+    alwaysInclude,
+    columns: 1,
+    formType,
+    name: `Test Custom Form for ${formType} ${alwaysInclude ? '' : 'NOT '}included`
+  });
 
-  visit('/admin/custom-forms');
-  click('button:contains(new form)');
-  andThen(function() {
-    if (assert) {
-      assert.equal(find('.view-current-title').text(), 'New Custom Form', 'New custom form edit page displays');
-      assert.equal(currentURL(), '/admin/custom-forms/edit/new', 'Navigated to create new custom form');
-    }
+  return cf.save();
 
-    fillIn('.custom-form-name input', `Test Custom Form for ${formType} ${alwaysInclude ? '' : 'NOT '}included`);
-    fillIn('.custom-form-columns input', '2');
-    select('.custom-form-type', formType);
-    if (alwaysInclude) {
-      click('.js-custom-form-always-include input');
-    }
-    click('.panel-footer button:contains(Add)');
-    waitToAppear('.modal-dialog');
-  });
-  andThen(function() {
-    if (assert) {
-      assert.equal(find('.modal-title').text(), 'Form Saved', 'Form is saved');
-    }
-
-    click('.modal-footer button:contains(Ok)');
-    addField('Header', 'Create a Pizza', header);
-  });
-  andThen(function() {
-    addField('Checkbox', 'Pizza Toppings', toppings);
-  });
-  andThen(function() {
-    addField('Dropdown', 'Pizza Crust', crusts);
-  });
-  andThen(function() {
-    addField('Radio', 'Dessert', desserts);
-  });
-  andThen(function() {
-    addField('Text', 'Beverage');
-  });
-  andThen(function() {
-    addField('Large Text', 'Special Instructions');
-  });
-  andThen(function() {
-    click('button:contains(Update)');
-    waitToAppear('.modal-dialog');
-  });
-  andThen(function() {
-    if (assert) {
-      assert.equal(find('.modal-title').text(), 'Form Saved', 'Form is updated');
-    }
-
-    click('.modal-footer button:contains(Ok)');
-  });
+  // function addField(fieldType, label, values) {
+  //   click('button:contains(Add Field)');
+  //   waitToAppear('.modal-dialog');
+  //   andThen(function() {
+  //     if (assert) {
+  //       assert.equal(find('.modal-title').text(), 'Add Value', 'Add Value modal displays.');
+  //     }
+  //
+  //     select('.custom-field-select', fieldType);
+  //     fillIn('.custom-field-label input', label);
+  //     fillIn('.custom-field-colspan input', '1');
+  //   });
+  //   if (values) {
+  //     if (assert) {
+  //       click('button:contains(Add Value)');
+  //       andThen(function() {
+  //         fillIn('.custom-field-value:last', 'Delete Me');
+  //       });
+  //       andThen(function() {
+  //         assert.equal(find('.custom-field-value:contains(Delete Me)').length, 0, 'Field value successfully added');
+  //         click('button.delete-field-value');
+  //       });
+  //       andThen(function() {
+  //         assert.equal(find('.custom-field-value:contains(Delete Me)').length, 0, 'Field value successfully deleted');
+  //       });
+  //     }
+  //     andThen(function() {
+  //       values.forEach(function(value) {
+  //         click('button:contains(Add Value)');
+  //         andThen(function() {
+  //           fillIn('.custom-field-value:last', value);
+  //         });
+  //       });
+  //     });
+  //   }
+  //   andThen(function() {
+  //     click('.modal-footer button:contains(Add)');
+  //   });
+  // }
+  //
+  // visit('/admin/custom-forms');
+  // click('button:contains(new form)');
+  // andThen(function() {
+  //   if (assert) {
+  //     assert.equal(find('.view-current-title').text(), 'New Custom Form', 'New custom form edit page displays');
+  //     assert.equal(currentURL(), '/admin/custom-forms/edit/new', 'Navigated to create new custom form');
+  //   }
+  //
+  //   fillIn('.custom-form-name input', `Test Custom Form for ${formType} ${alwaysInclude ? '' : 'NOT '}included`);
+  //   fillIn('.custom-form-columns input', '2');
+  //   select('.custom-form-type', formType);
+  //   if (alwaysInclude) {
+  //     click('.js-custom-form-always-include input');
+  //   }
+  //   click('.panel-footer button:contains(Add)');
+  //   waitToAppear('.modal-dialog');
+  // });
+  // andThen(function() {
+  //   if (assert) {
+  //     assert.equal(find('.modal-title').text(), 'Form Saved', 'Form is saved');
+  //   }
+  //
+  //   click('.modal-footer button:contains(Ok)');
+  //   addField('Header', 'Create a Pizza', header);
+  // });
+  // andThen(function() {
+  //   addField('Checkbox', 'Pizza Toppings', toppings);
+  // });
+  // andThen(function() {
+  //   addField('Dropdown', 'Pizza Crust', crusts);
+  // });
+  // andThen(function() {
+  //   addField('Radio', 'Dessert', desserts);
+  // });
+  // andThen(function() {
+  //   addField('Text', 'Beverage');
+  // });
+  // andThen(function() {
+  //   addField('Large Text', 'Special Instructions');
+  // });
+  // andThen(function() {
+  //   click('button:contains(Update)');
+  //   waitToAppear('.modal-dialog');
+  // });
+  // andThen(function() {
+  //   if (assert) {
+  //     assert.equal(find('.modal-title').text(), 'Form Saved', 'Form is updated');
+  //   }
+  //
+  //   click('.modal-footer button:contains(Ok)');
+  // });
 });
 
 Ember.Test.registerAsyncHelper('checkCustomFormIsDisplayed', function(app, assert, header, inTabs) {
